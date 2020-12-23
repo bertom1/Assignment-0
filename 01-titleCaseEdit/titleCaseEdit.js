af4b289576fd85a90cb213742df1fd51a0deaa58
Roberto C Melchor <rmelchor151@gmail.com>
2020-12-23 15:38:22 -0500

    update 01 and 04

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,7 +1,7 @@
 function titleCaseEdit(title) {
   // Insert code here;
-  var titleArr = title.split(" ");
-  for (var i = 0; i < titleArr.length; i++){
+  let titleArr = title.split(" ");
+  for (let i = 0; i < titleArr.length; i++){
     if (titleArr[i].charAt(0) != titleArr[i].charAt(0).toUpperCase()) {
       titleArr[i] = titleArr[i].charAt(0).toUpperCase() + titleArr[i].substring(1);
     }
--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,11 +1,11 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
   let sum = 0;
-  for (let i = 0; i < arr.length; i++){
-    if (arr[i] === true || arr[i] === false) {
-      sum += 1;
+  arr.forEach(element => {
+    if (element === true || element === false) {
+      sum++;
     }
-  }
+  });
   return sum;
 }
 
